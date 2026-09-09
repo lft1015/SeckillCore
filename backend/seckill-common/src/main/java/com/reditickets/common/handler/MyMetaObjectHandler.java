0ecf5b613ff8cd13bd4d5c29845430e1219a8279
gugu <1809233970@qq.com>
2026-09-09 16:37:56 +0800

    add()：创建时间与更新时间的自动填充配置

---
+++ backend/seckill-common/src/main/java/com/reditickets/common/handler/MyMetaObjectHandler.java
@@ -0,0 +1,32 @@
+package com.reditickets.common.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * 自定义元对象处理类
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    /**
+     * 插入操作时填充字段
+     */
+    public void insertFill(MetaObject metaObject) {
+        LocalDateTime now = LocalDateTime.now();
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, now);
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, now);
+    }
+
+    @Override
+    /**
+     * 更新操作时填充字段
+     */
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
